b7255bff5707d0ddf3cd67bfa0d8e5b8f9df972a
karmaqq <esat0738@gmail.com>
2025-02-07 12:02:57 +0300

    Açıklayıcı bir commit mesajı

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 // --------------- TEMEL DEĞERLER ---------------
-let karma = 0;
+let karma = 1000000000000000;
 let kps = 1;
 
 const habitats = [
-  { name: "Kümes", price: 1000, image: "img/habitats/coop.png", level: 0 },
-  { name: "Çatı", price: 10000, image: "img/habitats/roof.png", level: 0 },
-  { name: "Gölet", price: 50000, image: "img/habitats/lake.png", level: 0 },
-  { name: "Kara Orman", price: 120000, image: "img/habitats/nightforest.png", level: 0 },
-  { name: "Tropikal", price: 500000, image: "img/habitats/tropical.png", level: 0 },
-  { name: "Dağ", price: 1400000, image: "img/habitats/mountain.png", level: 0 }
+  { name: "Kümes", price: 100, image: "img/habitats/coop.png", level: 0 },
+  { name: "Çatı", price: 500, image: "img/habitats/roof.png", level: 0 },
+  { name: "Gölet", price: 2000, image: "img/habitats/lake.png", level: 0 },
+  { name: "Kara Orman", price: 10000, image: "img/habitats/nightforest.png", level: 0 },
+  { name: "Tropikal", price: 50000, image: "img/habitats/tropical.png", level: 0 },
+  { name: "Dağ", price: 250000, image: "img/habitats/mountain.png", level: 0 }
 ];
 
 const animals = {
-  tavuk: { price: 10, kps: 1, quantity: 0, image: "img/animal/chicken.png", sound: "sounds/chicken.mp3" },
-  güvercin: { price: 50, kps: 5, quantity: 0, image: "img/animal/pigeon.png", sound: "sounds/pigeon.mp3" },
-  ördek: { price: 200, kps: 20, quantity: 0, image: "img/animal/duck.png", sound: "sounds/duck.mp3" },
-  baykuş: { price: 1000, kps: 100, quantity: 0, image: "img/animal/owl.png", sound: "sounds/owl.mp3" },
-  papağan: { price: 5000, kps: 500, quantity: 0, image: "img/animal/parrot.png", sound: "sounds/parrot.mp3" },
-  phoenix: { price: 25000, kps: 2500, quantity: 0, image: "img/animal/phoenix.png", sound: "sounds/phoenix.mp3" }
+  tavuk: { price: 1, kps: 1, quantity: 0, image: "img/animal/chicken.png", sound: "sounds/chicken.mp3" },
+  güvercin: { price: 40, kps: 2, quantity: 0, image: "img/animal/pigeon.png", sound: "sounds/pigeon.mp3" },
+  ördek: { price: 160, kps: 4, quantity: 0, image: "img/animal/duck.png", sound: "sounds/duck.mp3" },
+  baykuş: { price: 650, kps: 8, quantity: 0, image: "img/animal/owl.png", sound: "sounds/owl.mp3" },
+  papağan: { price: 10500, kps: 52, quantity: 0, image: "img/animal/parrot.png", sound: "sounds/parrot.mp3" },
+  phoenix: { price: 42000, kps: 100, quantity: 0, image: "img/animal/phoenix.png", sound: "sounds/phoenix.mp3" }
 };
 
 // --------------- YUMURTA TIKLAMA ---------------
